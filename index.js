a466fc5f57e70835b56fb7f49b3e979fc9ce3c72
oier-jyb666 <jyb666_lzs@163.com>
2023-06-20 11:26:20 +0800

    重写 config 读取

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const session  = require('express-session');
 let Express = require('express');
 global.app = Express();
 
+const fs = require('fs');
+const path = require('path');
+
+// 读取 config
+var config = JSON.parse(fs.readFileSync(path.join(__dirname + "/config", 'config.json'), {encoding: 'utf8',flag: 'r'}).toString());
+
 // Set template engine ejs
 app.set("view engine","ejs");
+app.set("views", __dirname + "/views");
 // Set assets dir
-app.use(Express.static(__dirname + '/static', { maxAge: '1y' }));
+app.use(Express.static(__dirname + "/static", { maxAge: '1y' }));
 // Set Session
 app.use(session ({
-  secret:'dn65f61f68yn1fd6w64e8h',
+  secret: config.serve.session_token,
   resave:false,
   saveUninitialized: true,
   cookie: {secure:false, maxAge: 3*24*60*60} /* 第一个参数：只有在https才可以访问cookie；第二个参数：设置cookie的过期时间 */
@@ -17,28 +24,18 @@ app.use(session ({
 
 app.server = http.createServer(app);
 
-// 全局变量
-var confing = {
-  title: 'ApexOJ',
-  root: '', // 域名
-  menu: {
-    list: [{name: '首页', url: '/'}],
-    userlist: [{name: '个人主页', url: '/user'}]
-  }
-};
-
 // 解析 POST 提交过来的表单数据
 app.use(Express.urlencoded({ extended: false }));
 
 // api HTML 模块
 app.get('/', function(req, res){
   res.render('index',{
-    confing: confing,
+    config: config.main,
     session: req.session,
     title: '首页'
   });
 });
-__dirname
+
 var cors = require('cors');
 app.use(cors());
 
@@ -61,6 +58,6 @@ io.on('connection', (socket) => {
   });
 });
 
-app.server.listen(5246, function(){
-  console.log('[index.js]: listening on *:5246');
+app.server.listen(config.serve.post, function(){
+  console.log('[index.js]: listening on *:' + config.serve.post);
 });
