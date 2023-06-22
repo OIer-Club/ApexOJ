3e2c1e9e4876d5eff74e47188ccb829df64a62ea
oier-jyb666 <jyb666_lzs@163.com>
2023-06-22 20:11:14 +0800

    修复了一些已知问题

--- static/js/apexoj.js
+++ static/js/apexoj.js
@@ -14,21 +14,30 @@ var _GET = (function () {
   }
 })();
 
+var isload = false;
+
 function user_login() {
-  $.post('/api/user/login', {
-    "username": document.getElementById("login_username").value,
-    "password": document.getElementById("login_password").value
-  }, function (data) {
-    if (data.code == 'OK') {
-      swalx("登入成功", 2);
-      location.reload();
-    } else if (data.code == 'NO') {
-      swalx("账号/密码错误", 2);
-      document.getElementById("login_password").value = "";
-    } else {
-      swalx("未知错误,请重试", 10);
-    }
-  });
+  if (!isload) {
+    isload = true;
+    $.post('/api/user/login', {
+      "username": document.getElementById("login_username").value,
+      "password": document.getElementById("login_password").value
+    }, function (data) {
+      if (data.code == 'OK') {
+        swalx("登入成功", 2);
+        location.reload();
+      } else if (data.code == 'NO') {
+        isload = false;
+        swalx("账号/密码错误", 2);
+        document.getElementById("login_password").value = "";
+      } else {
+        isload = false;
+        swalx("未知错误,请重试", 10);
+      }
+    });
+  } else {
+    swalx("验证中，请耐心等待。", 5);
+  }
 }
 
 function user_login_enter() {
@@ -40,25 +49,32 @@ function user_login_exit() {
 }
 
 function user_register() {
-  if (document.getElementById("register_password").value != document.getElementById("register_repassword").value) {
-    swalx("两次密码不正确", 2);
-    document.getElementById("register_password").value = "";
-    document.getElementById("register_repassword").value = "";
+  if (!isload) {
+    if (document.getElementById("register_password").value != document.getElementById("register_repassword").value) {
+      swalx("两次密码不正确", 2);
+      document.getElementById("register_password").value = "";
+      document.getElementById("register_repassword").value = "";
+    } else {
+      isload = true;
+      swalx("验证中，请耐心等待。", 5);
+      $.post('/api/user/register', {
+        "username": document.getElementById("register_username").value,
+        "password": document.getElementById("register_password").value,
+        "email": document.getElementById("register_email").value
+      }, function (data) {
+        if (data.code == 'OK') {
+          window.location.href = "/user/email_key?token=" + data.token;
+        } else if (data.code == "NO") {
+          isload = false;
+          swalx(data.message, 2);
+        } else {
+          isload = false;
+          swalx("未知错误,请重试" + data.message, 10);
+        }
+      });
+    }
   } else {
     swalx("验证中，请耐心等待。", 5);
-    $.post('/api/user/register', {
-      "username": document.getElementById("register_username").value,
-      "password": document.getElementById("register_password").value,
-      "email": document.getElementById("register_email").value
-    }, function (data) {
-      if (data.code == 'OK') {
-        window.location.href = "/user/email_key?token=" + data.token;
-      } else if (data.code == "NO") {
-        swalx(data.message, 2);
-      } else {
-        swalx("未知错误,请重试" + data.message, 10);
-      }
-    });
   }
 }
 
